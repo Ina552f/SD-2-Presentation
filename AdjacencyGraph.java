cd084696b45229633e4e0591f8e1fe3b4b30ce3e
Kristina <72916303+Ina552f@users.noreply.github.com>
2021-06-04 13:30:45 +0200

    Update AdjacencyGraph.java
    Code originally created by Line Reinhardt.
    In this code I'm trying to convert from an adjacency matrix to adjacency list. 
    - Kristina Rasmussen (68909)

--- AdjacencyGraph.java
+++ AdjacencyGraph.java
@@ -1,87 +1,111 @@
+import java.io.OptionalDataException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.HashSet;
+import java.util.Set;
 
-public class MatrixGraph {
-    int[][] matrixegdegraph;
-    int[][] matrixweightgraph;
-    MatrixGraph(int vertices){
-        matrixegdegraph=new int[vertices][vertices];
-        matrixweightgraph =new int[vertices][vertices];
-    }
-    public void addEdge(int from, int to, int weight)
-    {
-        matrixegdegraph[from][to]=1;
-        matrixweightgraph[from][to]=weight;
-    }
-    public void MSTPrims(){
-        int[] Distance =new int[matrixegdegraph.length];
-        int[] predecessor = new int[matrixegdegraph.length];
-        boolean[] visited =  new boolean[matrixegdegraph.length];
-        MinHeap<Pair> Q =new MinHeap<>();
-        PriorityQueue<Pair> PQ = new PriorityQueue<>(); // offer (add) poll (extactmin)
+public class AdjacencyGraph {
+    ArrayList<Vertex> vertices;
 
-        ArrayList<Pair> VertexPairs=new ArrayList<>();
-        Arrays.fill(Distance, Integer.MAX_VALUE);
-        Arrays.fill(predecessor,-1);
-        Arrays.fill(visited,false);
-        if (matrixegdegraph.length>0)
-            Distance[0]=0;
-        for(int i=0;i<matrixegdegraph.length;i++) {
-            VertexPairs.add(new Pair(Distance[i], i));
-            Q.Insert(VertexPairs.get(i));
+    public AdjacencyGraph(){
+        vertices=new ArrayList<Vertex>();
+    }
+
+    public void addVertex(Vertex v){
+        vertices.add(v);
+    }
+    public void addEdge(Vertex f,Vertex t, Integer w){          // from,to,weight
+        if(!(vertices.contains(f) && vertices.contains(t)) ) {
+            System.out.println("Vertex not in graph");
+            return;
         }
-        int MST=0;
-        while(!Q.isEmpty()){
-            Pair u=Q.extractMin();
-            for(int v=0;v<matrixegdegraph.length;v++){
-                if(matrixegdegraph[u.index][v]==1 && matrixweightgraph[u.index][v]<Distance[v])
+        // It is here that I make the edges/cities bidirectional by adding Edge1.
+        Edge Edge=new Edge(f,t,w);
+        Edge Edge1=new Edge(t,f,w);
+    }
+    public void MSTPrims()  {
+        //Setup for the algorithm
+//        int[] Distance =new int[AdjacencyGraph.length]; //slet
+//        int[] Predecessor = new int[AdjacencyGraph.length]; //slet
+        MinHeap<Vertex> Q = new MinHeap<>();                          // Adjacency doesn't want pair, but do not know what is supposed to be instead. fordi vertex indeholder det hele.
+        ArrayList<Vertex> VertexInMST = new ArrayList<>();              // vertices included in MST
+        if(vertices.size()>0)
+            vertices.get(0).distance=0;         // sætter roden til 0
+        for(int i=0;i<vertices.size();i++) {
+            Q.Insert(vertices.get(i));
+        }
+
+        // The algorithm
+        int MST = 0;
+        while(!Q.isEmpty()) {
+            Vertex minVertex = Q.extractMin();
+            VertexInMST.add(minVertex);
+            MST += minVertex.distance;
+            for (Edge outEdge: minVertex.OutEdges) {
+                if(!VertexInMST.contains(outEdge.to)  && outEdge.weight < outEdge.to.distance)
                 {
-                    if(!visited[v]) {
-                        Distance[v] = matrixweightgraph[u.index][v];
-                        predecessor[v] = u.index;
-                        int pos = Q.getPosition(VertexPairs.get(v));
-                        VertexPairs.get(v).distance = matrixweightgraph[u.index][v];
+                        outEdge.to.distance = outEdge.weight;
+                        outEdge.to.prev = minVertex;
+                        int pos = Q.getPosition(outEdge.to);
                         Q.decreasekey(pos);
-                    }
                 }
             }
-            MST+=Distance[u.index];
         }
-        System.out.println("Minimum spanning tree Dsitance: " +MST);
-        for(int i=0; i< matrixegdegraph.length;i++)
-        {
-            System.out.println(" parent "+ predecessor[i] + " to " + i + " EdgeWeight: " + Distance[i]);
-        }
+        System.out.println("Minimum spanning tree distance: " +MST);
+            for(int i = 1; i< VertexInMST.size(); i++)
+            {
+                System.out.println(" parent "+ VertexInMST.get(i).prev.name + " to " + VertexInMST.get(i).name +" EdgeWeight: "+ VertexInMST.get(i).distance);
+            }
     }
-
-
-    public void printGraph(){
-        for(int fromi =0; fromi< matrixegdegraph.length; fromi++){
-            System.out.println("Edges from vertex "+ fromi);
-            for(int toj=0;toj<matrixegdegraph.length;toj++){
-                if(matrixegdegraph[fromi][toj]==1)
-                {
-                    System.out.print( " To "+ toj);
-                    System.out.println(" weight "+ matrixweightgraph[fromi][toj]);
-                }
+    public  void PrintGraph(){
+        for (Vertex vertex : vertices) {
+            System.out.println(" From: " + vertex.name);
+            for (int j = 0; j < vertex.OutEdges.size(); j++) {
+                Edge currentEdge = vertex.OutEdges.get(j);
+                System.out.println(" To: " + currentEdge.to.name + " km: " + currentEdge.weight);
             }
             System.out.println(" ");
         }
     }
 }
-
-class Pair implements Comparable<Pair>{
+class Vertex implements Comparable<Vertex>{
+    String name;
+    ArrayList<Edge> OutEdges;
+    Integer distance = Integer.MAX_VALUE;
+    Vertex prev = null;                     // Added null
+    public Vertex(String id){
+        name=id;
+        OutEdges=new ArrayList<Edge>();
+    }
+    public void addOutEdge(Edge e) {
+        OutEdges.add(e);
+    }
+    @Override
+    public int compareTo(Vertex o) {
+        return this.distance.compareTo(o.distance);
+    }
+}
+class Edge{
+    Integer weight;
+    Vertex from;
+    Vertex to;
+    public Edge(Vertex from, Vertex to, Integer cost){
+        this.from=from;
+        this.to=to;
+        this.weight=cost;
+        this.from.addOutEdge(this);
+    }
+}
+// This class ties the distance with index so they can be compared
+class Pair implements Comparable<Pair> {
    Integer distance;
    Integer index;
-
-   public Pair(Integer distance, Integer index){
+   public Pair(Integer distance, Integer index) {
        this.distance=distance;
-       this.index = index;
+       this.index=index;
    }
-
-   @Override
-   public int compareTo(Pair p){
-       return this.distance.compareTo(p.distance);
-   }
+    @Override
+    public int compareTo(Pair p) {
+        return this.distance.compareTo(p.distance);
+    }
 }
