c49336218ca47aed60f21d30733ec68917d6fec6
Kristina <72916303+Ina552f@users.noreply.github.com>
2021-06-07 20:43:36 +0200

    Update AdjacencyGraph.java

--- AdjacencyGraph.java
+++ AdjacencyGraph.java
@@ -1,8 +1,4 @@
-import java.io.OptionalDataException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class AdjacencyGraph {
     ArrayList<Vertex> vertices;
